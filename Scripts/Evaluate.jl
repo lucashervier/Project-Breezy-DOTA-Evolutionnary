d4591ff10d11c7b51de802a104ace7ffe4b91cc9
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-06-08 18:18:58 +0200

    CGP Agent improved with a new fitness and early stopping

--- Scripts/Evaluate.jl
+++ Scripts/Evaluate.jl
@@ -1,5 +1,6 @@
 # we import the list of our features into a dict to map array indexes
 const FEATURES_MAP = JSON.parsefile("features_list2.json")
+const TIME_TO_KILL = 120.0
 
 """
 Helper function to get the fitness whatever agent you are using. For now,
@@ -9,4 +10,28 @@ function Fitness(lastState::Array{Float64}{1})
 	# array index in Julia start at 1
 	gold = lastState[FEATURES_MAP["gold"]+1]
 	return gold
+end
+
+function Fitness1(lastState::Array{Float64}{1})
+	netWorth = lastState[FEATURES_MAP["net worth"]+1]
+	lastHits = lastState[FEATURES_MAP["last hits"]+1]
+	denies = lastState[FEATURES_MAP["last hits"]+1]
+	towerHealth = lastState[FEATURES_MAP["bad tower health"]+1]
+	maxTowerHealth = lastState[FEATURES_MAP["bad tower max health"]+1]
+	ratioTower = (maxTowerHealth-towerHealth)/maxTowerHealth
+	reward = netWorth + 100*lastHits + 100*denies + 2000*ratioTower
+	return reward 
+end
+
+"""
+Helper function to know if you should quit the game earlier than the GameOver
+"""
+function EarlyStop(lastState::Array{Float64}{1})
+	dotaTime = lastState[FEATURES_MAP["dota time"]+1]
+	lastHits = lastState[FEATURES_MAP["last hits"]+1]
+	if (dotaTime>TIME_TO_KILL&&lastHits<1)
+		return true
+	else
+		return false
+	end
 end
---
+++ Scripts/MainCGPV1.jl
@@ -0,0 +1,88 @@
+cd("C:/Lucas_Hervier/Lucas_Hervier/Documents/Cours/3A/SFE/Dota_challenge/Project-Breezy-DOTA-Evolutionnary") 
+## Import necessary package
+using HTTP
+using Random
+using JSON
+using CartesianGeneticProgramming
+using Cambrian
+using ArgParse
+using Sockets
+using Formatting
+include("Scripts/Evaluate.jl")
+include("Scripts/CGPAgentV1.jl")
+
+## Settings
+## Necessary settings
+s = ArgParseSettings()
+@add_arg_table! s begin
+    "--breezyIp"
+    help = "breezy server IP adress"
+    default = "127.0.0.1"
+    "--breezyPort"
+    help = "breezy server port number"
+    default = "8085"
+    "--agentIp"
+    help = "agent server IP adress"
+    default = "127.0.0.1"
+    "--agentPort"
+    help = "agent server port number"
+    default = "8086"
+    "--startData"
+    help = "the initial number of games launch when the agent is started"
+    arg_type = Dict{String}{Any}
+    default = Dict(
+                "agent"=> "Sample Random Agent",
+                "size"=> 1
+            )
+    "--cfg"
+    help = "configuration script"
+    default = "Config/CGPconfigV1.yaml"
+end
+
+
+args = parse_args(ARGS, s)
+cfg = get_config(args["cfg"])
+
+# add to cfg the number of input(i.e nb of feature) and output
+cfg["n_in"] = 310
+cfg["n_out"] = 30
+
+cfg["n_game"] = 0
+
+"""
+Declare variables global that you want the agent server to have access to.
+"""
+global breezyIp
+global breezyPort
+global agentIp
+global agentPort
+global startData
+global last_features
+global server
+global individual
+
+breezyIp = args["breezyIp"]
+breezyPort = args["breezyPort"]
+agentIp = args["agentIp"]
+agentPort = args["agentPort"]
+startData = args["startData"]
+# to be able to evaluate the fitness
+last_features = Dict("no_lastfeat_fornow"=>0)
+# the server will be reinitialize when playing Dota
+server = "whatever"
+# 
+individual = "not_initialized"
+# # test of the PlayDota function
+# fitnesses = []
+# for i in 1:3
+#     fit = PlayDota()
+#     push!(fitnesses,fit)
+# end
+# println(fitnesses)
+
+e = Cambrian.Evolution(CGPInd, cfg;
+                     populate=Populate,
+                     evaluate=Evaluate)
+Cambrian.run!(e)
+best = sort(e.population)[end]
+println("Final fitness: ", best.fitness[1])
