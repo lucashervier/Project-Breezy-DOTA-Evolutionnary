07c9a9e3f0db47c8155e1fd10bef28f66951470a
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-06-09 17:03:05 +0200

    Improvment of  EarlyStopping

--- Scripts/Evaluate.jl
+++ Scripts/Evaluate.jl
@@ -1,6 +1,6 @@
 # we import the list of our features into a dict to map array indexes
 const FEATURES_MAP = JSON.parsefile("features_list2.json")
-const TIME_TO_KILL = 120.0
+const TIME_TO_KILL = Dict(180.0=>1,300.0=>2,420.0=>3) 
 
 """
 Helper function to get the fitness whatever agent you are using. For now,
@@ -20,6 +20,7 @@ function Fitness1(lastState::Array{Float64}{1})
 	maxTowerHealth = lastState[FEATURES_MAP["bad tower max health"]+1]
 	ratioTower = (maxTowerHealth-towerHealth)/maxTowerHealth
 	reward = netWorth + 100*lastHits + 100*denies + 2000*ratioTower
+	# reward = netWorth + 100*lastHits + 100*denies + 400*trunc(ratioTower/0.2)
 	return reward 
 end
 
@@ -29,9 +30,10 @@ Helper function to know if you should quit the game earlier than the GameOver
 function EarlyStop(lastState::Array{Float64}{1})
 	dotaTime = lastState[FEATURES_MAP["dota time"]+1]
 	lastHits = lastState[FEATURES_MAP["last hits"]+1]
-	if (dotaTime>TIME_TO_KILL&&lastHits<1)
-		return true
-	else
-		return false
+	for (timeCreep,killedCreep) in TIME_TO_KILL
+		if (dotaTime>timeCreep&&lastHits<killedCreep)
+			return true
+		end
 	end
+	return false
 end
